3c4de6cc1dc3341900abf9a36ceb84aee3d9b91e
Mark Struck <markstruck@comcast.net>
2018-05-30 13:02:28 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+import {addTextToBody} from './utils.js';
+import {controls} from './constrols';
+
+
+const initApp = () => {
+    addTextToBody('Initilizaing the application.');
+};
+
+initApp();
+
+
+
---
+++ controls.js
@@ -0,0 +1,15 @@
+class RadioButton {
+    constructor(id) {
+
+        // add event listeners on the radio button
+        // the ability to create radio groups
+        // the ability to retrieve the currently selected value
+        // from the radio button.
+
+        
+    
+    }
+
+
+
+}
